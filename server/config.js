ebd25906fbe38e727c46aa191382cd69d3f0eede
purwa-mohod <149687939+purwa-mohod@users.noreply.github.com>
2025-04-09 23:07:25 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 
-export const SERVER_URL_FASTAPI = 'https://lifely-p9p9.onrender.com';
+export const SERVER_URL_FASTAPI = 'https://localhost:8000';
 export const LOCAL_SERVER_URL_FASTAPI = 'http://127.0.0.1:8000';
 
